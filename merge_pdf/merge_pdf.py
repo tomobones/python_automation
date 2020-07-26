544ae1630061ab45d8fb49ce857565454350a98b
tomobones <thomas.vogg@posteo.de>
2020-07-26 16:36:36 +0200

    some error handling

--- merge_pdf/merge_pdf.py
+++ merge_pdf/merge_pdf.py
@@ -1,17 +1,25 @@
 # this script reads files from files.txt
 # files.txt has the format of the output of the ls command
 
-
+import sys
 from PyPDF2 import PdfFileMerger, PdfFileReader
  
 mergedObject = PdfFileMerger()
 #pdfs_to_merge = ["test_file_1.pdf",
 #                "test_file_2.pdf"]
 
-file = open('files.txt', 'r')
+try:
+    file = open('files.txt', 'r')
+except FileNotFoundError:
+    print("FileNotFoundError: please provide 'files.txt', eg as an output of 'ls'", file=sys.stderr)
+
+    sys.exit(1)
+    
 pdfs_to_merge = file.readlines()
  
 for pdf_file in pdfs_to_merge:
     mergedObject.append(PdfFileReader(pdf_file.replace("\n",""), 'rb'))
  
 mergedObject.write("merged.pdf")
+
+file.close()
