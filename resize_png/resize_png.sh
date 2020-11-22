11608eaa08355b60636d3c9ad874fc2c191f1abe
tomobones <thomas.vogg@posteo.de>
2020-11-22 16:48:44 +0100

    fully implemented a script to automated png image resize. passing files as argument and through pipe is possible. in advance of the resizing process, a backup of each file is done by appending width and height to the name. the size will be halfed in width and height.

---
+++ resize_png/resize_png.sh
@@ -0,0 +1,57 @@
+#!/usr/local/bin/python3
+
+"""
+resizing png files to half size in height and length
+"""
+
+import sys
+import os
+from PIL import Image
+
+def parse_arguments():
+    files = []
+    if len(sys.argv) > 1:
+        for element in sys.argv[1:]: files.append(element)
+    return files
+
+def parse_stdinput():
+    files = []
+    if not sys.stdin.isatty():
+        for line in sys.stdin:
+            files.append(line.rstrip())
+    return files
+
+def check_extensions(files, extension):
+    if files == []: return False
+    for file in files:
+        if file[-3:] != extension:
+            return False
+    return True
+
+def resize_png(path):
+    try:
+        png = Image.open(path)
+    except IOError:
+        sys.stderr.write("exception: coudn't open " + path + "\n")
+        sys.stderr.flush()
+        sys.exit(1)
+
+    width, height = png.size
+    new_png = png.resize((int(width * .5), int(height * .5)))
+    png.save(path[:-4] + '_' + str(width) + 'x' + str(height) + '.png')
+    new_png.save(path)
+
+
+def main():
+    files = list(parse_arguments())
+    if files == []:
+        files = list(parse_stdinput())
+
+    if not check_extensions(files, 'png'):
+        sys.stderr.write("error: no valid files passed.\n")
+    else:
+        for file in files:
+            resize_png(file)
+            sys.stdout.write("success: made a backup and resized " + file + "\n")
+
+main()
