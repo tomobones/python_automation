aed9e388a6792c86eafb494055452e85bb12eada
Thomas Vogg <thomas.vogg@posteo.de>
2021-09-05 18:38:05 +0200

    Added functionality: Sum of all outputs of a specific month

--- finance/finance.sh
+++ finance/finance.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 
+import datetime as dt
 import pandas as pd
 
-file_path = "/home/tomo/Dropbox/Markdown/Finanzen/ausgaben.csv"
-df_data = pd.read_csv(file_path, delimiter=';')
+path = "/home/tomo/Dropbox/Markdown/Finanzen/ausgaben.csv"
+format = "%Y.%m.%d %H:%M"
+name_date = "DatumZeit"
+df = pd.read_csv(path, delimiter=';')
+df["Datum Zeit"] = pd.to_datetime(df["Datum Zeit"], format=format)
+df.set_index("Datum Zeit", inplace=True)
+df["Betrag"] = pd.to_numeric(df["Betrag"])
+df["Tags"] = df["Tags"].str.replace(" ", "").str.slice(1).str.split(pat="#")
 
-print(df_data)
+#print(df.dtypes)
+
+essen = ["nahrung", "essen", "eat", "gastro"]
+bildung = ["bildung", "bücher", "coding", "udemy"]
+
+
+datum = "2021-sept"
+sum = df.loc[datum]["Betrag"].sum()
+print(df[datum]["Tags"])
+print(f"Summe vom {datum} ist {sum:.2f} EUR")
